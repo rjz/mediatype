6a02e449870b91d34db47120a95671302319235e
rjz <rj@rjzaworski.com>
2017-07-29 10:41:10 -0700

    Adds quality support

--- mediatype.go
+++ mediatype.go
@@ -1,39 +1,93 @@
 package mediatype
 
 import (
+	"errors"
+	"fmt"
 	"mime"
 	"net/http"
+	"sort"
+	"strconv"
 	"strings"
 )
 
-func hasMediaRange(mediaRange, mimetype string) bool {
-	for _, v := range strings.Split(mediaRange, ",") {
-		t, _, err := mime.ParseMediaType(v)
-		if err != nil {
-			break
-		}
+var ErrInvalid = errors.New("invalid media-range")
 
-		if t == mimetype || t == "*/*" || (strings.HasSuffix(t, "/*") && strings.HasPrefix(mimetype, t[0:len(t)-2])) {
+// DefaultMimeType is applied to requests without a Content-type header
+const DefaultMimeType = "application/octet-stream"
+
+// IsAccepted validates the mimetype against the specified pattern
+func IsAccepted(mimetype, pattern string) bool {
+	if pattern == "*/*" {
+		return true
+	}
+
+	delimIndex := strings.Index(mimetype, "/")
+	if strings.EqualFold(pattern[0:delimIndex], mimetype[0:delimIndex]) {
+		if pattern[delimIndex:] == "/*" {
 			return true
 		}
+		return strings.EqualFold(pattern[delimIndex:], mimetype[delimIndex:])
 	}
 	return false
 }
 
-// Determine whether the server can respond with a mime-type allowed by the
-// request's `accept` header
-func Accepts(r *http.Request, mimetype string) bool {
-	return hasMediaRange(r.Header.Get("Accept"), mimetype)
+// Prefers returns a list of accepted mime-types ordered by quality
+func Prefers(request *http.Request) ([]string, error) {
+	mediaRange := request.Header.Get("Accept")
+	acceptedTypes := make(map[string]string)
+
+	var qualities []string
+	for i, v := range strings.Split(mediaRange, ",") {
+		t, details, err := mime.ParseMediaType(v)
+		if err != nil {
+			return nil, ErrInvalid
+		}
+
+		quality := float64(1)
+		if q, ok := details["q"]; ok {
+			qf, err := strconv.ParseFloat(q, 32)
+			if err != nil || qf < 0 || qf > 1 {
+				return nil, ErrInvalid
+			}
+
+			quality = qf
+		}
+		qualStr := fmt.Sprintf("%0.2f#%d", quality, i)
+		acceptedTypes[qualStr] = t
+		qualities = append(qualities, qualStr)
+	}
+	sort.Sort(sort.Reverse(sort.StringSlice(qualities)))
+	fmt.Println(qualities)
+
+	var prefs []string
+	for _, k := range qualities {
+		prefs = append(prefs, acceptedTypes[k])
+	}
+
+	return prefs, nil
 }
 
-// Determine whether the request `content-type` includes a server-acceptable
-// mime-type
-//
-// Failure should yield an HTTP 415 (`http.StatusUnsupportedMediaType`)
-func HasContentType(r *http.Request, mimetype string) bool {
-	contentType := r.Header.Get("Content-type")
-	if contentType == "" {
-		return mimetype == "application/octet-stream"
+// Accepts verifies that a request will accept the specified mimetype
+func Accepts(request *http.Request, mimetype string) (bool, error) {
+	mediaRange := request.Header.Get("Accept")
+	for _, v := range strings.Split(mediaRange, ",") {
+		t, _, err := mime.ParseMediaType(v)
+		if err != nil {
+			return false, ErrInvalid
+		}
+
+		if IsAccepted(mimetype, t) {
+			return true, nil
+		}
 	}
-	return hasMediaRange(contentType, mimetype)
+	return false, nil
+}
+
+// HasContentType verifies that the request matches the specified mimetype
+func HasContentType(request *http.Request, mimetype string) bool {
+	contentType := request.Header.Get("Content-type")
+	if contentType == "" {
+		return mimetype == DefaultMimeType
+	}
+	return strings.EqualFold(contentType, mimetype)
 }
--- mediatype_test.go
+++ mediatype_test.go
@@ -3,19 +3,63 @@ package mediatype_test
 import (
 	"github.com/rjz/mediatype"
 	"net/http"
+	"strings"
 	"testing"
 )
 
-func req(mediaRange string) *http.Request {
+func TestPrefersInvalid(t *testing.T) {
+	tests := []string{
+		"text/foo/bar",
+		"text/plain;q=asdklj",
+		"text/plain;q=14",
+	}
+
+	for _, test := range tests {
+		mediaRange := test
+		r, _ := http.NewRequest("GET", "http://example.com", nil)
+		r.Header.Set("Accept", mediaRange)
+
+		if _, err := mediatype.Prefers(r); err == nil {
+			t.Errorf("Expected error from invalid Accept header '%s'", mediaRange)
+		}
+	}
+}
+
+func TestPrefers(t *testing.T) {
+	tests := [...][2]string{
+		{"text/json;q=0.8, text/plain;q=0.5", "text/json,text/plain"},
+		{"text/json;q=0.8, text/plain", "text/plain,text/json"},
+		{"text/json, text/plain", "text/plain,text/json"},
+	}
+
+	for _, test := range tests {
+		mediaRange := test[0]
+		expected := test[1]
+
+		r, _ := http.NewRequest("GET", "http://example.com", nil)
+		r.Header.Set("Accept", mediaRange)
+
+		prefs, _ := mediatype.Prefers(r)
+		if strings.Join(prefs, ",") != expected {
+			t.Errorf("Expected '%s' to equal '%s'", strings.Join(prefs, ","), expected)
+		}
+	}
+}
+
+func TestAcceptsInvalid(t *testing.T) {
+	mediaRange := "text/foo/bar"
 	r, _ := http.NewRequest("GET", "http://example.com", nil)
 	r.Header.Set("Accept", mediaRange)
-	r.Header.Set("Content-type", mediaRange)
-	return r
+
+	if _, err := mediatype.Accepts(r, "application/json"); err == nil {
+		t.Errorf("Expected error from invalid Accept header '%s'", mediaRange)
+	}
 }
 
 func TestAccepts(t *testing.T) {
 	tests := [...][2]string{
 		{"text/json, text/plain", "text/json"},
+		{"text/json, application/octet-stream, text/plain;q=0.8", "text/json"},
 		{"*/*", "text/json"},
 		{"text/*", "text/json"},
 	}
@@ -23,8 +67,10 @@ func TestAccepts(t *testing.T) {
 	for _, test := range tests {
 		mediaRange := test[0]
 		mimetype := test[1]
+		r, _ := http.NewRequest("GET", "http://example.com", nil)
+		r.Header.Set("Accept", mediaRange)
 
-		if !mediatype.Accepts(req(mediaRange), mimetype) {
+		if ok, _ := mediatype.Accepts(r, mimetype); !ok {
 			t.Errorf("Expected '%s' to accept '%s'", mediaRange, mimetype)
 		}
 	}
@@ -39,23 +85,30 @@ func TestNotAccepts(t *testing.T) {
 	for _, test := range tests {
 		mediaRange := test[0]
 		mimetype := test[1]
-		if mediatype.Accepts(req(mediaRange), mimetype) {
+		r, _ := http.NewRequest("GET", "http://example.com", nil)
+		r.Header.Set("Accept", mediaRange)
+
+		if ok, _ := mediatype.Accepts(r, mimetype); ok {
 			t.Errorf("Expected '%s' to reject '%s'", mediaRange, mimetype)
 		}
 	}
 }
 
-func TestContentType(t *testing.T) {
+func TestHasContentType(t *testing.T) {
 	tests := [...][2]string{
-		{"text/json, text/plain", "text/json"},
-		{"", "application/octet-stream"},
+		{"application/json", "application/json"},
+		{"application/octet-stream", mediatype.DefaultMimeType},
+		{"", mediatype.DefaultMimeType},
 	}
 
 	for _, test := range tests {
-		mediaRange := test[0]
+		contentType := test[0]
 		mimetype := test[1]
-		if !mediatype.HasContentType(req(mediaRange), mimetype) {
-			t.Errorf("Expected '%s' to have content-type '%s'", mediaRange, mimetype)
+		r, _ := http.NewRequest("GET", "http://example.com", nil)
+		r.Header.Set("Content-type", contentType)
+
+		if !mediatype.HasContentType(r, mimetype) {
+			t.Errorf("Expected '%s' to have content-type '%s'", contentType, mimetype)
 		}
 	}
 }
